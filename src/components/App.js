4b1b6470f674b01a8c0e2467e412a593b66a69df
SourabhKanase <141937826+SourabhKanase@users.noreply.github.com>
2023-12-24 09:59:26 +0000

    x

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,9 @@ import '../styles/App.css';
 
 const App = () => {
   return (
-        <h1>
+        
           <p>I am learning React. My life is getting better</p>
-          </h1>
+       
   )
 }
 
